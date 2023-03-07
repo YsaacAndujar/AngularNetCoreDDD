2c597fa423deab5b7359db971c6bec43e10d79ea
Tyrone09876543 <rubik09876543@gmail.com>
2023-03-07 14:08:31 -0400

    Agregar archivos de proyecto.

---
+++ Domain/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Car
+    {
+        //public int Id { get; set; }
+
+        //todo: brand
+        //todo: brand
+
+        public int Year { get; set; }
+
+    }
+}
